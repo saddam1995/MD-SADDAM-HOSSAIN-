c4c3fa04c102437c21af6f6607fe1f258954b5fa
saddam1995 <70446157+saddam1995@users.noreply.github.com>
2021-04-24 03:07:30 +0600

    Initialize chrome-api.archive.org

---
+++ src/javascript/debug.js
@@ -0,0 +1,30 @@
+/*jslint node: true */
+/*global console */
+
+"use strict";
+
+/*
+	Debug info logger
+*/
+function Debug() {
+	var self = this,
+		isEnabled = false;
+
+	/*
+		Enable the logging
+	*/
+	this.enable = function enable(status) {
+		isEnabled = status;
+	};
+
+	/**
+	 * Log text in the console
+	 * @param {string} text
+	 */
+	this.log = function log(text) {
+
+		if (isEnabled === true && text !== undefined) {
+			console.log(text);
+		}
+	};
+}

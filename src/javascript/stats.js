50ae76ddee2cd178ab046869f2ecfe9e191be907
saddam1995 <70446157+saddam1995@users.noreply.github.com>
2021-04-24 03:07:30 +0600

    Initialize manifest-chrome-api-permission

---
+++ src/javascript/stats.js
@@ -0,0 +1,69 @@
+/*jslint node: true */
+/*global browser, debug */
+
+"use strict";
+
+function Stats() {
+	var self = this,
+		statsEnabled = false,
+		defaults = {
+			pagesArchived: 0
+		},
+		stats = {};
+
+	/**
+	 * Load stats from storage
+	 * @param {boolean} statsEnabled
+	 */
+	this.load = function load(statsEnabled) {
+		self.statsEnabled = statsEnabled;
+
+		// Load stats if they are enabled
+		if (self.statsEnabled === true) {
+
+			browser.storage.sync.get(defaults, function (data) {
+				self.loaded = true;
+				self.stats = data;
+
+				debug.log('stats loaded');
+			});
+
+		}
+
+	};
+
+	/**
+	 * Return stats 
+	 * @return {object} stats
+	 */
+	this.get = function get() {
+
+		// Return stats if they are enabled
+		if (self.statsEnabled === true) {
+			return self.stats.pagesArchived;
+
+		} else {
+			return defaults.pagesArchived;
+		}
+	};
+
+	/*
+		Update stats 
+	*/
+	this.update = function update() {
+
+		// Update stats if they are enabled
+		if (self.statsEnabled === true) {
+			self.stats.pagesArchived += 1;
+
+			browser.storage.sync.set({
+				'pagesArchived': self.stats.pagesArchived
+			}, function () {
+
+				debug.log('stats updated');
+
+			});
+		}
+	};
+
+}
---
+++ src/javascript/validate.js
@@ -0,0 +1,44 @@
+/*jslint node: true */
+/*global debug, global */
+
+"use strict";
+
+/**
+ * Validate a URL or IP address (IPv4)
+ * @param {string} url
+ * @param {callable} callback
+ */
+function validate(url, callback) {
+	var valid = true,
+		hostname,
+		urlRegex = new RegExp(global.urlRegex);
+
+	debug.log('Vaildating URL: ' + url);
+
+	try {
+		// Validate URL format
+		if (urlRegex.test(url) === false) {
+			throw new Error('URL format is not valid: ' + url);
+		}
+
+		// Get hostname with regex
+		hostname = url.match(urlRegex);
+
+		// Check blacklist for hostname 
+		if (global.hostNameBlacklist.includes(hostname[1])) {
+			throw new Error('URL hostname blacklisted: ' + hostname[1]);
+		}
+
+		debug.log('URL is valid: ' + url);
+
+	} catch (exception) {
+		valid = false;
+		debug.log(exception.message);
+	}
+
+	/**
+	 * @callback validate~callback
+	 * @param {boolean} valid.
+	 */
+	callback(valid);
+}
